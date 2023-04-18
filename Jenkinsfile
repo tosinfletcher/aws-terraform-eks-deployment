a130f5750556d0b16b61305cae3b4846830846f8
tosinfletcher <tosinfletcher@gmail.com>
2023-04-17 21:14:32 -0600

    Modified resource name and added Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,34 @@
+pipeline {
+    parameters {
+    string(name: 'BRANCH', defaultValue: 'master', description: 'Branch to Build')
+    }
+    agent any
+
+    stages {
+        stage('Deploy EKS Cluster') {
+            steps {
+                withAWS(credentials: 'aws_cred', region: 'us-east-1') {
+                    sh 'terraform init'
+                    sh 'terraform validate'
+                    sh 'terraform plan'
+                    sh 'terraform apply --auto-approve'
+                }
+            }
+        }
+        stage('Connect Kubectl Agent to EKS') {
+            steps {
+                withAWS(credentials: 'aws_cred', region: 'us-east-1') {
+                    sh '$ aws eks update-kubeconfig --region us-east-1 --name eks_cluster'
+                    sh 'kubectl get svc'
+                }
+            }
+        }
+        stage('Deploy EKS int and ext Loadbalancer') {
+            steps {
+                withAWS(credentials: 'aws_cred', region: 'us-east-1') {
+                    sh 'kubectl apply -f ./internal_and_external_loadbalancer.yml'
+                }
+            }
+        }
+    }
+}
--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,52 +1,46 @@
-resource "aws_iam_role" "tfletcher_eks_role" {
-  name = "tfletcher_eks_role"
+resource "aws_iam_role" "eks_role" {
+  name = "eks_role"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "eks.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-
-resource "aws_iam_role_policy_attachment" "tfletcher_AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.tfletcher_eks_role.name
+  role       = aws_iam_role.eks_role.name
 }
 
-
-resource "aws_eks_cluster" "tfletcher_eks_cluster" {
-  name     = "tfletcher_eks_cluster"
-  role_arn = aws_iam_role.tfletcher_eks_role.arn
-
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "eks_cluster"
+  role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
     endpoint_public_access  = true
     endpoint_private_access = false
 
     subnet_ids = [
-      aws_subnet.tfletcher_public_1.id,
-      aws_subnet.tfletcher_public_2.id,
-      aws_subnet.tfletcher_public_3.id,
-      aws_subnet.tfletcher_private_1.id,
-      aws_subnet.tfletcher_private_2.id,
-    aws_subnet.tfletcher_private_3.id]
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id,
+      aws_subnet.public_3.id,
+      aws_subnet.private_1.id,
+      aws_subnet.private_2.id,
+      aws_subnet.private_3.id
+    ]
   }
-  depends_on = [aws_iam_role_policy_attachment.tfletcher_AmazonEKSClusterPolicy]
+  depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
 }
 
-
-
-resource "aws_iam_role" "tfletcher_eks_node_group_role" {
+resource "aws_iam_role" "eks_node_group_role" {
   name = "tfletcher_eks_node_group_role"
 
   assume_role_policy = jsonencode({
@@ -61,30 +55,26 @@ resource "aws_iam_role" "tfletcher_eks_node_group_role" {
   })
 }
 
-
-resource "aws_iam_role_policy_attachment" "tfletcher_AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.tfletcher_eks_node_group_role.name
+  role       = aws_iam_role.eks_node_group_role.name
 }
 
-
-resource "aws_iam_role_policy_attachment" "tfletcher_AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.tfletcher_eks_node_group_role.name
+  role       = aws_iam_role.eks_node_group_role.name
 }
 
-
-resource "aws_iam_role_policy_attachment" "tfletcher_AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.tfletcher_eks_node_group_role.name
+  role       = aws_iam_role.eks_node_group_role.name
 }
 
-
-resource "aws_eks_node_group" "tfletcher_eks_node_group" {
-  cluster_name    = aws_eks_cluster.tfletcher_eks_cluster.name
-  node_group_name = "tfletcher_eks_node_group"
-  node_role_arn   = aws_iam_role.tfletcher_eks_node_group_role.arn
-  subnet_ids      = [aws_subnet.tfletcher_private_1.id, aws_subnet.tfletcher_private_2.id, aws_subnet.tfletcher_private_3.id]
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks_node_group"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id, aws_subnet.private_3.id]
   instance_types  = ["t2.micro"]
   disk_size       = 8
 
@@ -99,8 +89,8 @@ resource "aws_eks_node_group" "tfletcher_eks_node_group" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.tfletcher_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.tfletcher_AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.tfletcher_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
 }
